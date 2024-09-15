b3808446cdad762ed59091b8ab0fdd56dda669ea
AbdurRahman2004 <ar978874@gmail.com>
2024-09-16 02:09:01 +0530

    16/09 Hero Section and side bar completed

--- README.md
+++ README.md
@@ -7,6 +7,4 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-=======
-# Ecommerce
->>>>>>> 78756142bec0d3fa3ef8d4a610e1e8ac1ef783e8
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from "../assets/assets"
-import { NavLink } from 'react-router-dom'
+import { NavLink , Link} from 'react-router-dom'
 
 const Navbar = () => {
+   
+  const [visible , setVisible ] = useState(false);
   return (
     <div className="flex item-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="" />
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+
+        {/* navlink will set active class in which navlink is currently use */}
+
            <NavLink to='/' className='flex flex-col items-center gap-1'>
             <p>Home</p>
             <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
@@ -35,6 +40,29 @@ const Navbar = () => {
               <p className='cursor-pointer hover:text-black'>Order</p>
               <p className='cursor-pointer hover:text-black'>Logout</p>
             </div>
+          </div>
+        </div>
+
+        <Link to="/cart" className="relative">
+        <img src={assets.cart_icon} alt="cart-icon" className='w-5 min-w-5'  />
+        <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} alt="menu-icon" className='w-5 sm:hidden' />
+
+        {/* setVisible state is use to make visible the sidebar on mobile screen */}
+        
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible? ` w-full`:`w-0`}`}>
+          <div className='flex flex-col text-gray-600'>
+            <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+              <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+              <p>Back</p>
+            </div>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>Home</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border'  to='/collection'>Collection</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>About</NavLink>
+            <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border'  to='/contact'>Contact</NavLink>
+
           </div>
         </div>
       </div>

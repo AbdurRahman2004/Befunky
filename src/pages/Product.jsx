8ab7dd19eb75744bd1239cc01cca0fd4f2c553ee
AbdurRahman2004 <ar978874@gmail.com>
2024-09-17 21:44:51 +0530

    Cart Page added 17/09

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -7,7 +7,7 @@ import RelatedProduct from '../components/RelatedProduct';
 const Product = () => {
 
   const { productId } = useParams();
-  const {products , currency } = useContext(ShopContext);
+  const {products , currency , addToCart } = useContext(ShopContext);
   const [productData , setProductData] = useState(false);
   const [image , setImage] = useState('')
   const [ size , setSize] = useState('')
@@ -56,7 +56,7 @@ const Product = () => {
                  {productData.sizes.map((item,index)=>(<button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? "border-orange-500" : ""}`} key={index}>{item}</button>))}
               </div>
             </div>
-            <button className='bg-black text-white py-3 px-8 text-sm active:bg-gray-700 '>ADD TO CART</button>
+            <button onClick={()=>{addToCart(productData._id,size)}} className='bg-black text-white py-3 px-8 text-sm active:bg-gray-700 '>ADD TO CART</button>
             <hr className='mt-8 sm:w-4/5 '/>
             <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
               <p >100% Original Product</p>

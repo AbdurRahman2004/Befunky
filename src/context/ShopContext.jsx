c6c4666c386e499d85cb632c81f01e4e464e2240
AbdurRahman2004 <ar978874@gmail.com>
2024-09-19 00:01:38 +0530

    Almost Finished

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import {toast} from "react-toastify"
+import {useNavigate} from "react-router-dom"
 
 export const ShopContext = createContext()
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
   const [search,setSearch] = useState('');
   const [showSearch , setShowSearch] = useState(false);
   const [cartItems ,setCartItems] = useState({})
+  const navigate = useNavigate()
 
    
   const addToCart = async (itemId ,size) =>{
@@ -48,7 +50,6 @@ const ShopContextProvider = (props) => {
             }
                  }
                  catch(errror){
-
                  }
             }
       }
@@ -63,8 +64,28 @@ const ShopContextProvider = (props) => {
        setCartItems(cartData);
      } 
 
+     const getCartAmount =  () =>{
+      let totalAmount = 0;
+      for(const items in cartItems){
+            console.log(items);
+            let itemInfo = products.find((product)=>product._id === items)
+            for(const item in cartItems[items]){
+                  try{
+                        if(cartItems[items][item] > 0){
+                              totalAmount += itemInfo.price * cartItems[items][item];
+                        }
+                  }
+                  catch(error){
+
+                  }
+            }
+      }
+
+      return totalAmount;
+     }
+
      const value = {
-           products ,updateQuantity, currency , delivery_fee , search , setSearch ,getCartCount, showSearch , setShowSearch,cartItems,addToCart
+           products ,getCartAmount,updateQuantity,navigate, currency , delivery_fee , search , setSearch ,getCartCount, showSearch , setShowSearch,cartItems,addToCart
      }
      return (
         <ShopContext.Provider value={value}>

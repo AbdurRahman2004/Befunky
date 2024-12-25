31ebdbd62465cd3365fadea442ea1e33b3080208
abdurrahman2004 <ar978874@gmail.com>
2024-12-25 17:18:31 +0530

    Add functionality fully completed

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const List = () => {
+  const [list , setList] = useState([])
+
+  const fetchList = async () => {
+
+  }
+
+  useEffect(()=>{
+    fetchList()
+  })
+  
   return (
     <div>List</div>
   )

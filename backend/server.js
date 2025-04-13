268e051dc35dcb436fa2bffa6d065f236849b688
abdurrahman2004 <ar978874@gmail.com>
2025-04-13 22:50:16 +0530

    Deploy issue fixed

--- backend/server.js
+++ backend/server.js
@@ -34,4 +34,4 @@ app.get('/',(req,res)=>{
    res.send("API WORKING")
 })
 
-app.listen(port,()=> console.log("Server started on port "+ port))
+app.listen(port,'0.0.0.0',()=> console.log("Server started on port "+ port))
